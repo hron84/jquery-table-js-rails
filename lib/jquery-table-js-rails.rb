14ee8c278353fc12bc42301bf926906cc4b0e6af
Garami Gábor <hron@hron.me>
2013-06-06 22:03:57 +0200

    Version number

--- lib/jquery-table-js-rails.rb
+++ lib/jquery-table-js-rails.rb
@@ -2,6 +2,7 @@ require 'rails/engine' unless defined?(::Rails)
 
 module JqueryTableJs
   module Rails
+    VERSION = File.read(File.expand_path('../../VERSION', __FILE__))
     class Engine < ::Rails::Engine
     end
   end
